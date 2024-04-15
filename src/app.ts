79e16a832c6dde77615811eb3f188cb8eeffef0c
Edwards Yan <yanedwards11@gmail.com>
2024-04-15 17:26:25 -0300

    create user, gyms and elationships

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,14 @@ import { PrismaClient } from "@prisma/client";
 export const app = fastify();
 const prisma = new PrismaClient();
 
-prisma.user.create({
-  data: {
-    name: "Yan Edwards",
-    email: "yan@gmail.com",
-  },
+// prisma.user.create({
+//   data: {
+//     name: "Yan Edwards",
+//     email: "yan@gmail.com",
+
+//   },
+// });
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello");
 });
