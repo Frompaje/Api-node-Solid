c0527fecc050d172b74856038a9086197eba4998
frompaje <yanedwards11@gmail.com>
2024-04-19 20:28:23 -0300

    Use case validate check in

--- src/repositories/prisma/check-ins-repository.ts
+++ src/repositories/prisma/check-ins-repository.ts
@@ -1,8 +1,10 @@
 import { CheckIn, Prisma } from "@prisma/client";
 
 export interface CheckInsRepository {
+  findById(id: string): Promise<CheckIn | null>;
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
   findByUserInOnDate(userId: string, date: Date): Promise<CheckIn | null>;
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
   countByUserId(userID: string): Promise<number>;
+  save(checkIn: CheckIn): Promise<CheckIn>;
 }
---
+++ src/use-cases/validate-check-in.ts
@@ -0,0 +1,34 @@
+import { ResourceNotFoundError } from "@/error/resource-not-found-error";
+import { CheckInsRepository } from "@/repositories/prisma/check-ins-repository";
+import { GymsRepository } from "@/repositories/prisma/gyms-repository";
+import { CheckIn } from "@prisma/client";
+
+interface ValidateCheckInUseCaseRequest {
+  checkInId: string;
+}
+
+interface ValidateCheckInUseCaseResponse {
+  checkIn: CheckIn;
+}
+
+export class ValidateCheckInUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    checkInId,
+  }: ValidateCheckInUseCaseRequest): Promise<ValidateCheckInUseCaseResponse> {
+    const checkIn = await this.checkInsRepository.findById(checkInId);
+
+    if (!checkIn) {
+      throw new ResourceNotFoundError();
+    }
+
+    checkIn.validated_at = new Date();
+
+    await this.checkInsRepository.save(checkIn);
+
+    return {
+      checkIn,
+    };
+  }
+}
