a99eb29e4080e6e4f81efb29921817bb788dabd0
frompaje <yanedwards11@gmail.com>
2024-04-23 14:37:05 -0300

    creating a test environment

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    environmentMatchGlobs: [
+      [
+        "/home/paje/Backend/Api-node-Solid/src/http/controllers/test/**",
+        "prisma",
+      ],
+    ],
+  },
 });
